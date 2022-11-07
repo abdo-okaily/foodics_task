fbb8a2536fb4ceaeb4aee606f8dba3388a3d9c20
Abdo okaily <abdo.is.master@gmail.com>
2022-11-07 19:06:51 +0200

    initial commit

---
+++ app/Models/prdouct_Ingredient.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class prdouct_Ingredient extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_11_06_124725_create_prdouct_ingredients_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prdouct_ingredients', function (Blueprint $table) {
+            $table->integer('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->integer('ingredient_id')->unsigned()->index();
+            $table->foreign('ingredient_id')->references('id')->on('ingredients')->onDelete('cascade');
+
+
+
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('ingredient_id');
+
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prdouct_ingredients');
+    }
+};
